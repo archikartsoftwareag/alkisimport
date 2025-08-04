33eec2e9245e464b3b393fbb8ab32dae2f5f5f38
Archikart Software AG <fsa@archikart.de>
2025-08-04 10:13:49 +0200

    Update 0_archikart.sql
    - fehlerhafter Tabellenname

--- postprocessing.d/4_nas2alb/0_archikart.sql
+++ postprocessing.d/4_nas2alb/0_archikart.sql
@@ -7,6 +7,6 @@ CREATE VIEW ak_fs_hist AS
 	SELECT a.gml_id, a.beginnt, a.endet
 	FROM ax_flurstueck a
 	LEFT JOIN ax_flurstueck b ON b.gml_id = a.gml_id AND b.endet IS NULL
-	INNER JOIN alkis_insert c ON c.featureid = a.gml_id
+	INNER JOIN ak_alkis_insert c ON c.featureid = a.gml_id
 	WHERE a.endet IS NOT NULL
 	AND b.gml_id IS NULL;
