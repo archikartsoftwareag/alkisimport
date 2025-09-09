d4f10cca9c0a3771bff3efbe00eced8aab02707c
Archikart Software AG <fsa@archikart.de>
2025-09-09 17:23:00 +0200

    Update 0_archikart.sql

--- preupdate.d/0_archikart.sql
+++ preupdate.d/0_archikart.sql
@@ -1,4 +1,4 @@
 SET search_path = :"alkis_schema", public;
 
-DROP VIEW ax_klassifizierung;
-DROP VIEW ax_tatsaechlichenutzung;
+DROP VIEW IF EXISTS ax_klassifizierung;
+DROP VIEW IF EXISTS ax_tatsaechlichenutzung;
